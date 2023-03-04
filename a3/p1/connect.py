923b377b8a9f6958ddb7dbe9446549558dbb1a42
Suraj Rathi <suraj.rathi00@gmail.com>
2023-03-04 11:31:37 +0530

    escape non readable (alphanumeric or space) characters

--- a3/p1/connect.py
+++ a3/p1/connect.py
@@ -5,6 +5,19 @@ server_ip = '10.21.235.155'
 server_port = 1023
 
 
+def readable_decode(inp: bytes) -> str:
+    out = ''
+    for byte in inp:
+        if (byte == ord(' ')) or \
+                (ord('0') <= byte <= ord('9')) or \
+                (ord('A') <= byte <= ord('Z')) or \
+                (ord('a') <= byte <= ord('z')):
+            out += chr(byte)
+        else:
+            out += f"\\x{byte:02x}"
+    return out
+
+
 def connect(username="test", password="test"):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((server_ip, server_port))
@@ -13,13 +26,16 @@ def connect(username="test", password="test"):
         s.send(password.encode('ASCII'))
         s.send(b'\n')
 
-        f = s.makefile('r', encoding='ASCII')
+        f = s.makefile('rb')
         for i in range(3):
             line = f.readline()
 
-        line = line[line.find('?') + 2:]
-        line = line[:line.find(" is not the correct password.")]
-        # print(f"_{line}_")
+        line = line[line.find(b'?') + 2:]
+        line = line[:line.find(b" is not the correct password.")]
+
+        # line = line.decode('ASCII')
+        line = readable_decode(line)
+
         return line
 
 
