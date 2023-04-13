e6f592fc013b02f25de12dba4a56c0369e76b963
Suraj Rathi <suraj.rathi00@gmail.com>
2023-04-13 19:20:08 +0530

    done with password suraj4

--- a5/hash_executer.c
+++ a5/hash_executer.c
@@ -1,17 +1,38 @@
 #include <stdio.h>
 
 #include <openssl/sha.h>
+#include <string.h>
 
-unsigned char true_hash[] = {124, 58, 177, 180, 135, 194, 12, 70, 113,
-                             73, 31, 63, 147, 188, 139, 184, 14,
-                             44, 22, 23,};
+// Generated offset: 6
+unsigned char true_hash[] = {24, 45, 223, 87, 45, 155, 197, 153, 202, 94, 20, 184, 67, 39, 243, 113, 185, 111, 45,
+                             202,};
 
 const size_t max_password_length = 40;
 
-int main() {
-    char input[max_password_length + 1];
-    fgets(input, max_password_length, stdin);
+struct two_nums {
+    int success;
+    int a, b;
+};
 
+struct two_nums get_nums() {
+    // a > b and a != 0, b != 0
+    struct two_nums nums = {0, 0, 0};
+    printf("Enter num1: ");
+    scanf("%i", &nums.a);
+    printf("Enter num2: ");
+    scanf("%i", &nums.b);
+    if (nums.a != 0 && nums.b != 0 && nums.a >= nums.b) {
+        nums.success = 1;
+    }
+    return nums;
+}
+
+int check_password() {
+
+    char input[max_password_length + 1];
+    memset(input, 0, max_password_length + 1);
+    printf("Enter the password: ");
+    fgets(input, max_password_length, stdin);
 
     unsigned char hash1[SHA_DIGEST_LENGTH]; // == 20
     unsigned char hash2[SHA_DIGEST_LENGTH]; // == 20
@@ -24,12 +45,74 @@ int main() {
     for (int i = 0; i < SHA_DIGEST_LENGTH; i++) {
         correct &= hash2[i] == true_hash[i];
     }
-    if (correct == 1) {
-        printf("The Password is Correct!");
-    } else {
-        printf("The Password is wrong.");
-    }
     unsigned int offset = hash1[SHA_DIGEST_LENGTH - 1] % 8;
+
+    if (correct == 1) {
+        return offset;
+    } else {
+        return -1;
+    }
+}
+
+typedef int (*func_type)(struct two_nums);
+
+void run_func(func_type func) {
+    struct two_nums nums = get_nums();
+    if (nums.success) {
+        printf("Output: %i\n", func(nums));
+    } else {
+        printf("Invalid Numbers inputed. Num1 must be greater than num2 and neither of them can be zero.");
+    }
+}
+
+
+int add(struct two_nums nums) {
+    return nums.a + nums.b;
+}
+
+int subtract(struct two_nums nums) {
+    return nums.a - nums.b;
+}
+
+int multiply(struct two_nums nums) {
+    return nums.a * nums.b;
+}
+
+int divide(struct two_nums nums) {
+    return nums.a / nums.b;
+}
+
+func_type func_table[8] = {
+        add,
+        add,
+        subtract,
+        subtract,
+        multiply,
+        multiply,
+        divide,
+        multiply,
+};
+
+int main() {
+    printf("(S)ecure function or (A)dd function: ");
+    char func[3];
+    fgets(func, 3, stdin);
+    int offset = -1;
+    switch (func[0]) {
+        case 'A':
+            run_func(&add);
+            break;
+        case 'S':
+            offset = check_password();
+            if (offset == -1) {
+                printf("User is not Authenticated\n");
+            } else {
+                run_func(func_table[offset]);
+            }
+            break;
+        default:
+            printf("Invalid Input.\n");
+    }
 
     return 0;
 }
--- a5/hash_generator.c
+++ a5/hash_generator.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 
 #include <openssl/sha.h>
+#include <string.h>
+
+const size_t max_password_length = 40;
 
 int main() {
-    const unsigned char str[] = "This is the super duper secret password!";
+    // const unsigned char str[] = "This is the super duper secret password!";
+    // const unsigned char str[max_password_length + 1] = "suraj";
+    unsigned char str[max_password_length + 1];
+    memset(str, 0, max_password_length + 1);
+
+    fgets(str, max_password_length + 1, stdin);
+
     unsigned char hash1[SHA_DIGEST_LENGTH]; // == 20
     unsigned char hash2[SHA_DIGEST_LENGTH]; // == 20
 
@@ -18,9 +27,9 @@ int main() {
 
 
     unsigned int offset = hash1[SHA_DIGEST_LENGTH - 1] % 8;
-    printf("Generated offset: %u\n", offset);
+    printf("// Generated offset: %u\n", offset);
 
-    printf("unsigned char second_hash[] = {");
+    printf("unsigned char true_hash[] = {");
     for (int i = 0; i < SHA_DIGEST_LENGTH; i++) {
         printf("%u, ", hash2[i]);
     }
